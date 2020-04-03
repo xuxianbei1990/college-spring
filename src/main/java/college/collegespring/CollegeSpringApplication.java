dc8e95278ab90c0b68d3657782ea0083c96e76f0
“许贤贝” <xu.xianbei@xyb2b.com>
2020-04-03 09:46:22 +0800

    Initial commit

---
+++ src/main/java/college/collegespring/CollegeSpringApplication.java
@@ -0,0 +1,13 @@
+package college.collegespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeSpringApplication.class, args);
+	}
+
+}
